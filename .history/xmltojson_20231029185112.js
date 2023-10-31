3ba173a8cbb94ea82fc15f7735da60a00a0539ea
samarth <samarth.shinde@devxconsultancy.com>
2023-10-31 19:16:44 +0530

    first

---
+++ .history/xmltojson_20231029185112.js
@@ -0,0 +1,42 @@
+const fs = require("fs");
+const xml2js = require("xml2js");
+
+// Read the XML file
+fs.readFile(
+	"input.xml",
+	"utf-8",
+	(error, data) => {
+		if (error) {
+			console.error("Failed to read file:", error);
+		} else {
+			// Parse the XML data
+			xml2js.parseString(data, (error, result) => {
+				if (error) {
+					console.error("Failed to parse XML:", error);
+				} else {
+					// Filter the JSON data
+					let filteredData = result.rss.channel[0].item.map((item) => {
+						return {
+              title: item.title[0],
+              pubDate: item.pubDate[0],
+              author: item["dc:creator"][0], // this depends on the structure of your XML
+              encodedContent: item["content:encoded"][0],
+              content: item.category,
+              // add other fields as necessary
+            };
+					});
+
+					// Convert the result to a JSON string
+					const json = JSON.stringify(filteredData, null, 2);
+
+					// Write the JSON data to a file
+					fs.writeFile("output.json", json, "utf8", (error) => {
+						if (error) {
+							console.error("Failed to write file:", error);
+						}
+					});
+				}
+			});
+		}
+	}
+);
---
+++ xmlToJsonImage.js
@@ -0,0 +1,41 @@
+const fs = require("fs");
+const xml2js = require("xml2js");
+
+// Read the XML file
+fs.readFile(
+	"/home/chirag/Documents/nextapp/firstimage.xml",
+	"utf-8",
+	(error, data) => {
+		if (error) {
+			console.error("Failed to read file:", error);
+		} else {
+			// Parse the XML data
+			xml2js.parseString(data, (error, result) => {
+				if (error) {
+					console.error("Failed to parse XML:", error);
+				} else {
+					// Filter the JSON data
+					let finalResult = result.rss.channel[0];
+					// let filteredData = result.rss.channel[0].item.map((item) => {
+					// 	return {
+
+					// 		// title: item.title[0],
+					// 		// author: item["dc:creator"][0], // this depends on the structure of your XM
+					// 		// add other fields as necessary
+					// 	};
+					// });
+
+					// Convert the result to a JSON string
+					const json = JSON.stringify(finalResult, null, 2);
+
+					// Write the JSON data to a file
+					fs.writeFile("firsimage.json", json, "utf8", (error) => {
+						if (error) {
+							console.error("Failed to write file:", error);
+						}
+					});
+				}
+			});
+		}
+	}
+);
